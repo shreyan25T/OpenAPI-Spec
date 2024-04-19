e6547328fce7ba4ef55e7136c99085079b5cf69f
shreyan25T <shreyan.shetty@tigeranalytics.com>
2024-04-19 17:17:39 +0530

    added toast message in frontend

--- frontend/src/components/OpenAPISpecReader.js
+++ frontend/src/components/OpenAPISpecReader.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const OpenAPISpecReader = () => {
   const [specData, setSpecData] = useState('');
@@ -24,37 +26,45 @@ const OpenAPISpecReader = () => {
         }
       });
 
-      let fileContent = response.data.data.spec_content;
-      console.log('spec',response.data.data.spec_content)
-      // Remove double quotes from the content
-      fileContent = fileContent.replace(/"/g, '');
-
-      // Update the text field with the content of the file
-      setSpecData(fileContent);
-      setspecPath(response.data.data.spec_file_path)
+      if (response.data.status === 'success') {
+        let fileContent = response.data.data.spec_content;
+        fileContent = fileContent.replace(/"/g, '');
+        setSpecData(fileContent);
+        setspecPath(response.data.data.spec_file_path);
+      } else {
+        toast.error(response.data.message); // Show error message from backend
+      }
     } catch (error) {
       console.error('Error uploading file:', error);
+      toast.error('Error uploading file.');
     }
   };
 
   const handleTest = async () => {
     try {
-      const response = await axios.post('http://127.0.0.1:8000/test', { spec_content: specData ,spec_file_path:specPath}, {
+      const response = await axios.post('http://127.0.0.1:8000/test', { spec_content: specData, spec_file_path: specPath }, {
         headers: {
           'Content-Type': 'application/json'
         }
       });
-      setTestResult(response.data.result);
 
+      if (response.data.status === 'success') {
+        setTestResult('Test cases got generated successfully.');
+      } else {
+        setTestResult('Error generating test cases.');
+      }
     } catch (error) {
       console.error('Error testing spec:', error);
+      setTestResult('Error testing spec.');
     }
   };
 
 
 
+
   return (
     <Grid container spacing={2} alignItems="center" justifyContent="center">
+      <ToastContainer />
       <Grid item xs={12}>
         <input type="file" onChange={handleUpload} style={{ display: 'none' }} id="upload-file-input" />
         <label htmlFor="upload-file-input">
