39d61351de74694516767ac6c3cc71ff1b933ff3
simranmaurya <simran209mr@gmail.com>
2024-04-19 18:12:36 +0530

    NavBar pic added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import logo from "../assests/TA-Logo.png";
 
 const Navbar = () => {
+  const Logout = () => {};
   return (
     <AppBar position="static">
       <Toolbar>
+        <Box
+          component="img"
+          sx={{ height: 45, marginRight: 3 }}
+          alt="Logo"
+          src={logo}
+        />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My App
+          OPENAPI SPEC APP
         </Typography>
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">About</Button>
-        <Button color="inherit">Contact</Button>
+        <Button color="inherit" onClick={Logout}>
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
