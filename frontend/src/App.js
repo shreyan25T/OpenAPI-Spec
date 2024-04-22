0a0063b9ed8b0fe210990fe5c76b97ef2caf76c4
simranmaurya <simran209mr@gmail.com>
2024-04-22 18:11:30 +0530

    OAuth SSO login/logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import OpenAPISpecReader from './components/OpenAPISpecReader';
-import Navbar from './components/Navbar';
+import React from "react";
+import OpenAPISpecReader from "./components/OpenAPISpecReader";
+import Navbar from "./components/Navbar";
+import LoginButton from "./login";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Typography, Box } from "@mui/material";
+import isAuthenticated from "./login";
+import isLoading from "./login";
 
 function App() {
+  const { user, loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  console.log("isAuthenticated ", isAuthenticated);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <React.Fragment>
-      <Navbar/>
-      <h1>OpenAPI Spec Reader</h1>
-      <OpenAPISpecReader />
-      </React.Fragment>
+      {user ? (
+        <React.Fragment>
+          <Navbar />
+          <h1>Hi {user.name}</h1>
+          <OpenAPISpecReader />
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <LoginButton />
+        </React.Fragment>
+      )}
+    </React.Fragment>
   );
 }
 
