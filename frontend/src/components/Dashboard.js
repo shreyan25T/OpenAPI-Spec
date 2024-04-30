f08d6e82e069a425ca50c67a323b34683e580939
shreyan25T <shreyan.shetty@tigeranalytics.com>
2024-04-30 18:57:50 +0530

    added icon route

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -2,36 +2,44 @@ import React from "react";
 import { Box } from "@mui/material";
 import Navbar from "./navbar/Navbar";
 import DashBoardCard from "./cards/DashBoardCard";
+import pytestlogo from "../assests/pytest.png"
+import locustlogo from "../assests/locust.png"
 
 const elements = [
-  { title: "Pytest", description: "Creates the pytest test files ",iconPath:require("/home/shreyan/react-prac/openspec/OpenAPI-Spec/frontend/src/assests/pytest.png"), routeTo: "/home" },
-  { title: "Locust Load", description: "Creates the locust test files", iconPath:require("/home/shreyan/react-prac/openspec/OpenAPI-Spec/frontend/src/assests/locust.png"), routeTo: "/locust" },
+    {
+        title: "Pytest", description: "Creates the pytest test files ",
+        iconPath: pytestlogo, routeTo: "/home"
+    },
+    {
+        title: "Locust Load", description: "Creates the locust test files", iconPath: locustlogo,
+        routeTo: "/locust"
+    },
 ];
 
 const DashBoard = () => {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <div className="grid grid-cols-1 gap-4 justify-items-center mt-20">
-        <Box
-          display={"flex"}
-          alignItems={"center"}
-          gap={"20px"}
-          flexDirection={"row"}
-        >
-          {elements.map((element) => (
-            <DashBoardCard
-              key={element.title}
-              title={element.title}
-              description={element.description}
-              icon={element.iconPath}
-              routeTo={element.routeTo}
-            />
-          ))}
-        </Box>
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Navbar />
+            <div className="grid grid-cols-1 gap-4 justify-items-center mt-20">
+                <Box
+                    display={"flex"}
+                    alignItems={"center"}
+                    gap={"20px"}
+                    flexDirection={"row"}
+                >
+                    {elements.map((element) => (
+                        <DashBoardCard
+                            key={element.title}
+                            title={element.title}
+                            description={element.description}
+                            icon={element.iconPath}
+                            routeTo={element.routeTo}
+                        />
+                    ))}
+                </Box>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default DashBoard;
