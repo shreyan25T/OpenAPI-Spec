fa46d00ade87b014d5da65816d008db733cbcbe4
simranmaurya <simran209mr@gmail.com>
2024-05-07 18:26:46 +0530

    delete button to the side

--- frontend/src/components/Selenium.js
+++ frontend/src/components/Selenium.js
@@ -1,98 +1,158 @@
-import React, { useState ,useCallback,useRef} from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import { Button, MenuItem, Select, TextField } from '@mui/material';
+import React, { useState, useCallback, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { Button, MenuItem, Select, TextField } from "@mui/material";
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css";
-import Navbar from './navbar/Navbar';
-import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Navbar from "./navbar/Navbar";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import "../assests/style.css";
 
 const Selenium = () => {
-   const gridRef = useRef();
-    const [url, setUrl] = useState('');
-    const [rowData, setRowData] = useState([
-        { id: 1, by: 'NAME', byInput: 'email', action: 'send_keys', actionInput: 'exampleInput' },
-        { id: 2, by: 'ID', byInput: 'exampleInputPassword1', action: 'send_keys', actionInput: 'password' }
-    ]);
+  const gridRef = useRef();
+  const [url, setUrl] = useState("");
+  const [rowData, setRowData] = useState([
+    {
+      id: 1,
+      by: "NAME",
+      byInput: "email",
+      action: "send_keys",
+      actionInput: "exampleInput",
+    },
+    {
+      id: 2,
+      by: "ID",
+      byInput: "exampleInputPassword1",
+      action: "send_keys",
+      actionInput: "password",
+    },
+  ]);
 
-
-    const handleAddRow = () => {
-        const newRow = { id: rowData.length + 1, by: 'NAME', byInput: '', action: 'send_keys', actionInput: '' };
-        setRowData([...rowData, newRow]);
+  const handleAddRow = () => {
+    const newRow = {
+      id: rowData.length + 1,
+      by: "NAME",
+      byInput: "Enter here",
+      action: "send_keys",
+      actionInput: "Enter here",
     };
-
-    const onRemoveSelected = useCallback(() => {
-        var selectedRowData = gridRef.current.api.getSelectedRows();
-        gridRef.current.api.applyTransaction({ remove: selectedRowData });
-        const updatedData = rowData.filter(row => !selectedRowData.some(selectedRow => selectedRow.id === row.id));
-        setRowData(updatedData);
-    }, [setRowData, rowData]);
-
-    const onTestButtonClick = async () => {
-      try {
-          const response = await fetch('http://127.0.0.1:8000/selenium/test', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(rowData),
-          });
-          const data = await response.json();
-          console.log(data);
-      } catch (error) {
-          console.error('Error:', error);
-      }
+    setRowData([...rowData, newRow]);
   };
 
-    const actionOptions = ['send_keys', 'clear', 'wait time', 'click'];
+  const handleDelete = (rowDataToDelete) => {
+    const updatedData = rowData.filter((row) => row !== rowDataToDelete);
+    setRowData(updatedData);
+  };
 
-    const columns = [
-        { headerName: 'By', field: 'by',checkboxSelection:true,headerCheckboxSelection:true, width: 150, editable: true, cellEditor: 'agSelectCellEditor', cellEditorParams: { values: ['NAME', 'ID', 'XPATH', 'CSS_SELECTOR', 'CLASS_NAME'] } },
-        { headerName: 'ByInput', field: 'byInput', width: 200, editable: true },
-        { headerName: 'Action', field: 'action', width: 150, editable: true, cellEditor: 'agSelectCellEditor', cellEditorParams: { values: actionOptions } },
-        { headerName: 'ActionInput', field: 'actionInput', width: 200, editable: true },
-    ];
+  const onTestButtonClick = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/selenium/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(rowData),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-    return (
-      <React.Fragment>
+  const actionOptions = ["send_keys", "clear", "wait time", "click"];
+
+  const columns = [
+    {
+      headerName: "By",
+      field: "by",
+      width: 150,
+      editable: true,
+      checkboxSelection: true,
+      cellEditor: "agSelectCellEditor",
+      cellEditorParams: {
+        values: ["NAME", "ID", "XPATH", "CSS_SELECTOR", "CLASS_NAME"],
+      },
+    },
+    { headerName: "ByInput", field: "byInput", width: 200, editable: true },
+    {
+      headerName: "Action",
+      field: "action",
+      width: 150,
+      editable: true,
+      cellEditor: "agSelectCellEditor",
+      cellEditorParams: { values: actionOptions },
+    },
+    {
+      headerName: "ActionInput",
+      field: "actionInput",
+      width: 200,
+      editable: true,
+    },
+    {
+      headerName: "Delete",
+      field: "delete",
+      width: 50,
+      height: 100,
+      cellRenderer: (params) => (
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => handleDelete(params.data)}
+          startIcon={<DeleteIcon />}
+        ></Button>
+      ),
+    },
+  ];
+
+  return (
+    <React.Fragment>
       <Navbar />
       <div className="grid grid-cols-1 gap-2 justify-items-center mt-20">
-            <TextField
-                label="Add driver.get URL here"
-                value={url}
-                onChange={(e) => setUrl(e.target.value)}
-                fullWidth
-                variant="outlined"
-                style={{ marginBottom: '10px' }}
-            />
-            <div className="ag-theme-quartz" style={{ width: '100%' }}>
-                <AgGridReact
-                    rowData={rowData}
-                    ref={gridRef}
-                    columnDefs={columns}
-                    domLayout='autoHeight'
-                    suppressMenu={true}
-                    suppressCellSelection={true}
-                    rowHeight={35}
-                    rowSelection={"multiple"}
-                    suppressRowClickSelection={true}
-                    defaultColDef={{ flex: 1, editable: true }}
-                />
-            </div>
-            <Button variant="contained" color="secondary" onClick={handleAddRow} startIcon={<AddIcon />}>
-                Add Row
-            </Button>
-            <Button variant="contained" color="secondary" onClick={onRemoveSelected} startIcon={<DeleteIcon />} style={{ marginTop: '5px' }}>
-                Delete Row
-            </Button>
-            <Button variant="contained" color="secondary" onClick={onTestButtonClick} style={{ marginTop: '5px' }}>
-                    Test Button
-                </Button>
+        <TextField
+          label="Add driver.get URL here"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          fullWidth
+          variant="outlined"
+          style={{ marginBottom: "10px" }}
+        />
+        <div className="ag-theme-quartz" style={{ width: "100%" }}>
+          <AgGridReact
+            rowData={rowData}
+            getRowNodeId={(data) => data.id}
+            ref={gridRef}
+            columnDefs={columns}
+            domLayout="autoHeight"
+            suppressMenu={true}
+            suppressCellSelection={true}
+            rowHeight={50}
+            animateRows={true}
+            suppressRowClickSelection={true}
+            defaultColDef={{ flex: 1, editable: true }}
+          />
         </div>
-        </React.Fragment>
-    );
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleAddRow}
+          startIcon={<AddIcon />}
+        >
+          Add Row
+        </Button>
+
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={onTestButtonClick}
+          style={{ marginTop: "5px" }}
+        >
+          Test Button
+        </Button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Selenium;
