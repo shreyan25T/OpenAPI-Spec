20ba83c9fffc473f4812302913dcc620756655af
abilashravi-ta <abilash.ravi@tigeranalytics.com>
2024-12-20 14:54:01 +0530

    Update simple_app.py

--- simple_app.py
+++ simple_app.py
@@ -1,19 +1,8 @@
-from fastapi import FastAPI, Request, Form
-from pydantic import BaseModel
-from typing_extensions import Annotated
+from fastapi import FastAPI, Request
 
 app = FastAPI()
-
-class FormData(BaseModel):
-    longitude: float
-    latitude: float
 
 @app.get('/home')
 async def gethome(request: Request):
     ctx = {'request': request}
-    return str(ctx)
-
-@app.post('/home')
-async def posthome(request: Request, data: Annotated[FormData, Form()]):
-    ctx = {'request': request, 'data': data}
     return str(ctx)
